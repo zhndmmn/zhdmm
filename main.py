70fdcd8c2c0d2bc5ee8db7bd3018abc38e35b674
zhndmmn <107850777+zhndmmn@users.noreply.github.com>
2022-06-20 16:29:53 +0600

    Update and rename mytgbot.py to main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import telebot
+from googletrans import Translator
+from telebot import types
+from pytube import YouTube
+
+
+t = '5432678033:AAHXyWowK4GL1aeo1omm8M1L6RDtYTLi3tA'
+bot = telebot.TeleBot(t)
+@bot.message_handler(commands=['start'])
+def start(msg):
+    bot.send_message(msg.chat.id,'cкинь ссылку')
+@bot.message_handler(content_types=['text'])
+def get_video(msg):
+    link = msg.text
+    video = YouTube(link)
+    filename = video.streams.filter(res='720p').first().default_filename
+    video.streams.filter(res='720p').first().download()
+    file = open(filename,'rb')
+    bot.send_video(msg.chat.id, file)
+    bot.send_message(msg.chat.id,'приятного')
+
+
+    print('done')
+
+bot.polling()
+    
