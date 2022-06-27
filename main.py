0724cc7dbf4c5daedbd0c4ded3bea7afe7492842
zhndmmn <107850777+zhndmmn@users.noreply.github.com>
2022-06-27 16:47:58 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,44 @@
 import telebot
+import random
 from telebot import types
 from pytube import YouTube
 import flask
 import os
 
-t = '5432678033:AAHXyWowK4GL1aeo1omm8M1L6RDtYTLi3tA'
-bot = telebot.TeleBot(t)
+# py -m pip install pytube
+
+server = flask.Flask(__name__)
+
+app_name = 'youtube-bot-downloader-1'
+token = '5432678033:AAHXyWowK4GL1aeo1omm8M1L6RDtYTLi3tA'
+bot = telebot.TeleBot(token)
+
 @bot.message_handler(commands=['start'])
 def start(msg):
-    bot.send_message(msg.chat.id,'cкинь ссылку')
+    bot.send_message(msg.chat.id, 'Отправьте ссылку на видео youtube')
+
 @bot.message_handler(content_types=['text'])
 def get_video(msg):
     link = msg.text
     video = YouTube(link)
     filename = video.streams.filter(res='720p').first().default_filename
     video.streams.filter(res='720p').first().download()
-    file = open(filename,'rb')
+    file = open(filename, 'rb')
     bot.send_video(msg.chat.id, file)
-    bot.send_message(msg.chat.id,'приятного')
+    bot.send_message(msg.chat.id, 'Приятного просмотра')
+
+@server.route('/' + token, methods=['POST'])
+def get_message():
+      bot.process_new_updates([types.Update.de_json(flask.request.stream.read().decode("utf-8"))])
+      return "!", 200
+
+@server.route('/', methods=["GET"])
+def index():
+    print("hello webhook!")
+    bot.remove_webhook()
+    bot.set_webhook(url=f"https://{app_name}.herokuapp.com/{token}")
+    return "Hello from Heroku!", 200
 
 
-    print('done')
-
-bot.infinity_polling(True)
-    
+if __name__ == '__main__':
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
